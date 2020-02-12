ce947e80b8c7f37d5bd75ddac9db50eaa54b1f0d
Josh Montoya <josh@montoya.io>
2020-02-12 10:44:11 -0800

    Implement syslog test

--- scribe_test.go
+++ scribe_test.go
@@ -1,6 +1,9 @@
 package scribe
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestScribe_New(t *testing.T) {
 	s := New("Testing")
@@ -17,13 +20,42 @@ func TestScribe_New_File(t *testing.T) {
 		err error
 	)
 
-	if f, err = NewFile("test", "./"); err != nil {
+	if err = os.MkdirAll("./test_data", 0744); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll("./test_data")
+
+	if f, err = NewFile("test", "./test_data"); err != nil {
 		t.Fatalf("error creating file writer: %v", err)
 	}
 
 	f.SetLineCapacity(1)
 
 	s := NewWithWriter(f, "Testing")
+	s.Notification("This is a notification log", nil)
+	s.Success("This is a success log", nil)
+	s.Warning("This is a warning log", nil)
+	s.Error("This is an error log", nil)
+	s.Debug("This is a debug log", nil)
+}
+
+func TestScribe_New_Syslog(t *testing.T) {
+	addr := os.Getenv("SCRIBE_SYSLOG_ADDR")
+	if len(addr) == 0 {
+		t.Fatal("no syslog address env found, please set SCRIBE_SYSLOG_ADDR")
+	}
+
+	var (
+		w   *Syslog
+		err error
+	)
+
+	if w, err = NewSyslog("test", addr); err != nil {
+		t.Fatalf("error creating syslog writer: %v", err)
+	}
+	defer w.Close()
+
+	s := NewWithWriter(w, "Testing")
 	s.Notification("This is a notification log", nil)
 	s.Success("This is a success log", nil)
 	s.Warning("This is a warning log", nil)
