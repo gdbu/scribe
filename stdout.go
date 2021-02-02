e98715e65a8b2a9f6017d1bf972e85c33e51ada9
Josh Montoya <josh@montoya.io>
2021-02-02 14:12:38 -0800

    Remove Stdout type

--- stdout.go
+++ stdout.go
@@ -3,14 +3,8 @@ package scribe
 import "os"
 
 // NewStdout will create a new instance of Stdout
-func NewStdout() *Stdout {
-	var s Stdout
-	s.Basic = NewBasic(os.Stdout)
-	s.Basic.SetSuffix("\n")
-	return &s
-}
-
-// Stdout is a Writer used to push scribe entries to std output
-type Stdout struct {
-	*Basic
+func NewStdout() *Basic {
+	b := NewBasic(os.Stdout)
+	b.SetSuffix("\n")
+	return b
 }
